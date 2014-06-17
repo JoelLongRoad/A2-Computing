66021c257d87f55e2608a5a242b8dc4b4b8a7b6d
Joel Butcher <34771@longroad.ac.uk>
2014-06-17 15:16:46 +0100

    Graphical User Interface starts

---
+++ Graphical Programming/PyQt_Basic.py
@@ -0,0 +1,16 @@
+import sys
+
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+
+class MainWindow(QMainWindow):
+    def __init(self):
+        super().__init__()
+        self.setWindowTitle("Hello World")
+
+if __name__ == "__main__":
+    application = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    window.raise_()
+    application.exec_()
